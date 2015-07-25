252eff86b3a5b53761b8f9449f7ee80605a1dffe
Lin Yiming <linym@jadenine.com>
2015-07-25 16:17:49 +0800

    ADD: pretty time

--- circle/src/main/java/com/jadenine/circle/ui/utils/TimeFormatUtils.java
+++ circle/src/main/java/com/jadenine/circle/ui/utils/TimeFormatUtils.java
@@ -1,15 +1,16 @@
 package com.jadenine.circle.ui.utils;
 
-import java.text.SimpleDateFormat;
+import org.ocpsoft.prettytime.PrettyTime;
+
 import java.util.Date;
 
 /**
  * Created by linym on 7/24/15.
  */
 public class TimeFormatUtils {
-    private final static SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd HH:mm:ss");
+    private final static PrettyTime prettyTimer = new PrettyTime();
 
     public static String getFormattedTime(long timestamp) {
-        return dateFormat.format(new Date(timestamp));
+        return prettyTimer.format(new Date(timestamp));
     }
 }
