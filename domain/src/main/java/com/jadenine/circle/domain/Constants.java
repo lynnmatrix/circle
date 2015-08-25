65bf3ccf2ff52ee519d185ca2108281ad4100a85
Lin Yiming <linym@jadenine.com>
2015-08-25 14:12:57 +0800

    VERSION 0.1.5

--- domain/src/main/java/com/jadenine/circle/domain/Constants.java
+++ domain/src/main/java/com/jadenine/circle/domain/Constants.java
@@ -6,11 +6,8 @@ package com.jadenine.circle.domain;
 public class Constants {
     public static final Integer PAGE_SIZE = 100;
     public static final int CAPABILITY = 2000;
-
-//    public static final String ENDPOINT_LOCAL = "http://192.168.9.117:8080";
-    public static final String ENDPOINT_LOCAL = "https://circle.jadenine.com.cn";
-//    public static final String ENDPOINT_AZURE = "https://circle.chinacloudsites.cn:443";
-    public static final String ENDPOINT_AZURE = "https://circle.jadenine.com.cn";
+    public static final String ENDPOINT_LOCAL = "http://192.168.9.117:8080";
+    public static final String ENDPOINT_AZURE = "https://circle.chinacloudsites.cn:443";
     public static final int COMPRESS_DST_WIDTH = 1024;
     public static final int COMPRESS_DST_HEIGHT = 512;
     public static final int TOP_K = 10;
