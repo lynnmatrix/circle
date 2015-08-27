c7d582788fe78e9acfc8960205b93722f7ded16c
qil <qil@corp.jadenine.com>
2015-08-27 22:59:10 +0800

    图片长按保存，修复打开图片一定几率黑屏

--- circle/src/main/java/com/jadenine/circle/ui/image/ImageActivity.java
+++ circle/src/main/java/com/jadenine/circle/ui/image/ImageActivity.java
@@ -1,18 +1,36 @@
 package com.jadenine.circle.ui.image;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DownloadManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.jadenine.circle.R;
 import com.jadenine.circle.ui.widgets.TouchImageView;
 import com.squareup.picasso.Picasso;
 
+import org.mockito.internal.util.io.IOUtil;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import uk.co.senab.photoview.PhotoView;
 
 /**
  * Created by linym on 8/27/15.
@@ -22,9 +40,11 @@ public class ImageActivity extends Activity {
     private static final String EXTRA_IMAGE = "image";
 
     @InjectView(R.id.image_view)
-    TouchImageView imageView;
+    PhotoView imageView;
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
+
+    private String mImageUri;
 
     public static void actionOpenFullImage(Context context, String imageUri) {
         Intent intent = new Intent(context, ImageActivity.class);
@@ -38,9 +58,9 @@ public class ImageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
         ButterKnife.inject(this);
-        String imageUri = getIntent().getStringExtra(EXTRA_IMAGE);
-        if(null != imageUri) {
-            Picasso.with(this).load(imageUri).into(imageView);
+        mImageUri = getIntent().getStringExtra(EXTRA_IMAGE);
+        if(null != mImageUri) {
+            Picasso.with(this).load(mImageUri).into(imageView);
         }
 
         toolbar.setNavigationIcon(R.drawable.ic_actionbar_back_light);
@@ -50,15 +70,100 @@ public class ImageActivity extends Activity {
                 onBackPressed();
             }
         });
+
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                showSaveImageDialog();
+                return true;
+            }
+        });
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
 
-        String imageUri = getIntent().getStringExtra(EXTRA_IMAGE);
-        if(null != imageUri) {
-            Picasso.with(this).load(imageUri).into(imageView);
+        mImageUri = getIntent().getStringExtra(EXTRA_IMAGE);
+        if(null != mImageUri) {
+            Picasso.with(this).load(mImageUri).into(imageView);
         }
+    }
+
+    private void showSaveImageDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setMessage(R.string.dialog_save_image_message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        saveImage(mImageUri);
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //DO NOTHING
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void saveImage(final String imageUri) {
+        new Thread() {
+            @Override
+            public void run() {
+                Bitmap bmp = null;
+                try {
+                    bmp = Picasso.with(ImageActivity.this).load(imageUri).get();
+                } catch (IOException ignore) {
+                }
+                if(bmp != null) {
+                    File savePath = Environment.getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_DOWNLOADS);
+                    savePath.mkdirs();
+                    Date dt = new Date(System.currentTimeMillis());
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmss", Locale.getDefault());
+                    String name = sdf.format(dt);
+                    File file = new File(savePath.getAbsolutePath(), name + ".png");
+                    int offset = 1;
+                    while(file.exists()) {
+                        offset++;
+                        file = new File(savePath.getAbsolutePath(), name + "_" + offset + ".png");
+                    }
+                    OutputStream out = null;
+                    boolean success;
+                    try {
+                        out = new FileOutputStream(file);
+                        success = bmp.compress(Bitmap.CompressFormat.PNG, 100, out);
+                    } catch (FileNotFoundException e) {
+                        success = false;
+                    } finally {
+                        IOUtil.closeQuietly(out);
+                    }
+                    if(success) {
+                        DownloadManager dm = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                        if(dm != null) {
+                            dm.addCompletedDownload(file.getName(), file.getName(),
+                                    false , "image/png",
+                                    file.getAbsolutePath(), file.length(), true);
+                        }
+                        showToast(R.string.save_image_success);
+                    } else {
+                        showToast(R.string.save_image_fail);
+                    }
+                } else {
+                    showToast(R.string.save_image_fail);
+                }
+            }
+        }.start();
+    }
+
+    private void showToast(final int resId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(ImageActivity.this, resId, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
